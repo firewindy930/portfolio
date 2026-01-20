984a3d925c457e78b38419204a9e02edc5ffc90c
firewindy930 <laos20823@gmail.com>
2026-01-20 03:50:31 -0800

    add coment

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,5 +1,5 @@
 import { nextJsConfig as baseNextJsConfig } from "@workspace/eslint-config/next-js";
-
+// this is the eslint config for the web app
 /** @type {import("eslint").Linter.Config} */
 const nextJsConfig = [
   ...baseNextJsConfig,
